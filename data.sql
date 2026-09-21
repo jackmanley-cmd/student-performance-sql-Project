d8f86261b22ba475b5b3a34664ec1c3fd2c5158a
jackmanley-cmd <jackmanleygolf@gmail.com>
2026-09-21 16:02:46 +0100

    Create data.sql
    Add student performance data

---
+++ data.sql
@@ -0,0 +1,50 @@
+-- Insert class information
+INSERT INTO classes (class_id, class_name)
+VALUES
+(1, 'Three'),
+(2, 'Four'),
+(3, 'Five'),
+(4, 'Six'),
+(5, 'Seven'),
+(6, 'Eight'),
+(7, 'Nine');
+
+
+-- Insert student performance data
+INSERT INTO students (student_id, name, class_id, mark, gender)
+VALUES
+(1, 'John Deo', 2, 75, 'female'),
+(2, 'Max Ruin', 1, 85, 'male'),
+(3, 'Arnold', 1, 55, 'male'),
+(4, 'Krish Star', 2, 60, 'female'),
+(5, 'John Mike', 2, 60, 'female'),
+(6, 'Alex John', 2, 55, 'male'),
+(7, 'My John Rob', 3, 78, 'male'),
+(8, 'Asruid', 3, 85, 'male'),
+(9, 'Tes Qry', 4, 78, NULL),
+(10, 'Big John', 2, 55, 'female'),
+(11, 'Ronald', 4, 89, 'female'),
+(12, 'Recky', 4, 94, 'female'),
+(13, 'Kty', 5, 88, 'female'),
+(14, 'Bigy', 5, 88, 'female'),
+(15, 'Tade Row', NULL, 88, 'male'),
+(16, 'Gimmy', 2, 88, 'male'),
+(17, 'Tumyu', 4, 54, 'male'),
+(18, 'Honny', 3, 75, 'male'),
+(19, 'Tinny', 7, 18, 'male'),
+(20, 'Jackly', 7, 65, 'female'),
+(21, 'Babby John', 2, 69, 'female'),
+(22, 'Reggid', 5, 55, 'female'),
+(23, 'Herod', 6, 79, 'male'),
+(24, 'Tiddy Now', 5, 78, 'male'),
+(25, 'Giff Tow', 5, 88, 'male'),
+(26, 'Crelea', 5, 79, 'male'),
+(27, NULL, 1, 81, NULL),
+(28, 'Rojj Base', 5, 86, 'female'),
+(29, 'Tess Played', 5, 55, 'male'),
+(30, 'Reppy Red', 4, 79, 'female'),
+(31, 'Marry Toeey', 2, 88, 'male'),
+(32, 'Binn Rott', 5, 90, 'female'),
+(33, 'Kenn Rein', 4, 96, 'female'),
+(34, 'Gain Toe', 5, 69, 'male'),
+(35, 'Rows Noump', 4, 88, 'female');
